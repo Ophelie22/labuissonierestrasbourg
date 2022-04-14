ebde043f8858fd3b14ac80c69f98412be7a5ce41
Juliette Vila <contactjuliettevila@gmail.com>
2022-04-14 17:41:00 +0200

    ajout du formaulaire category pour les differentes commissions

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -3,6 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Category;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ColorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class CategoryCrudController extends AbstractCrudController
@@ -12,14 +17,24 @@ class CategoryCrudController extends AbstractCrudController
         return Category::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
+        
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+                TextField::new('name', 'Nom de la commission'),
+                SlugField::new('slug')->setTargetFieldName('name'),
+                ColorField::new('color')
         ];
+        
     }
-    */
+    //  public function viewArticle(AdminContext $context): Response
+    // {
+    //     /** @var Article $article */
+    //     $article = $context->getEntity()->getInstance();
+
+    //     return $this->redirectToRoute('article_show', [
+    //         'slug' => $article->getSlug()
+    //     ]);
+    // }
 }
--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,11 +4,15 @@ namespace App\Controller\Admin;
 
 use Symfony\Component\HttpFoundation\Response;
 use App\Controller\Admin\DelegueCrudController;
+use App\Controller\Admin\CategoryCrudController;
+use App\Entity\Category;
+use App\Entity\Delegue;
 use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 
 
 class DashboardController extends AbstractDashboardController
@@ -20,8 +24,8 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-
-       $url =  $this->adminUrlGenerator->setController(DelegueCrudController::class)
+            //ca c'est juste pour récuperer notre URL()
+       $url =  $this->adminUrlGenerator->setController(CategoryCrudController::class)
         ->generateUrl();
         
         return $this->redirect($url);
@@ -36,7 +40,15 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToDashboard('Tableau de bord', 'fa fa-home');
+        yield MenuItem::subMenu('Commissions', 'fas fa-newspaper')->setSubItems([
+            //on utilise une methode stattique avec linktoCrud
+            MenuItem::linkToCrud(label:'Toutes les commissions', icon:'', entityFqcn: Category::class),
+            MenuItem::linkToCrud(label:'Ajouter', icon:'fas fa-plus', entityFqcn:Category::class)->setAction(actionName:Crud::PAGE_NEW),
+            MenuItem::linkToCrud(label:'Personne responsable de  commission', icon:'fas fa-list', entityFqcn:Delegue::class)
+        ]);
+            
+           
+        //yield MenuItem::subMenu('Delegue de commission', 'fas fa-newspaper', Delegue::class);
     }
 }
