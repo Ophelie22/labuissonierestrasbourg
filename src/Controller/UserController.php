3efee3990e7287121259b5fcd8f68ed08700a00f
Juliette Vila <contactjuliettevila@gmail.com>
2022-04-29 22:37:01 +0200

    ajout de l'entité article ou l'utilsateur deposeras ces documents

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -13,18 +13,34 @@ class UserController extends AbstractController
     //On va recuperer l'utilisateur courant
     
     #[Route('/utilisateur/edition/{id}', name: 'user.edit')]
-    public function edit(User $user): Response
+    public function edit(User $user, Request $request, EntityManagerInterface $manager): Response
     {
         // on va verifier que ce n'est pas un autre utilsateur et si il est connecte si c pas le cas on le renvoie sur la page login
         if (!$this->getUser()) {
             return $this->redirectToRoute('security.login');
         }
         // si le meme utilisateur qui est récupéré par rapport à notre id {id}
-        if (!$this->getUser() === $user) {
+        if (!$this->getUser() !== $user) {
             // si ce petit salopio essayes de modifier un autre profil que le sien on le renvoi a ces article ( Entity d'ailleur a creer)
             return $this->redirectToRoute('article.index');
         }
         $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $manager->persist($user);
+            $manager->flush();
+            
+            
+            $this->addFlash(
+                    'success',
+                    'Les informations de changement de votre compte ont bien été modifiées.'
+                );
+                return $this->redirectToRoute('article.index');
+            }
+        
+
 
         return $this->render('pages/user/edit.html.twig', [
             'form' => $form->createView(),
