3de01973c99f43588b9c911f6e217812c462da4b
Juliette Vila <contactjuliettevila@gmail.com>
2022-07-25 10:17:34 +0200

    home redirect

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,29 +2,49 @@
 
 namespace App\Controller;
 
-use App\Entity\Category;
+
 use App\Repository\ArticleRepository;
-use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'home.page')]
+    /**
+     * @Route("/home", name="home")
+     */
     public function home(ArticleRepository $articleRepository): Response
     {
-        return $this->render('pages/home.html.twig', [
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
             'articles'=> $articleRepository->findPublicArticle(4)
-            //'categories' => $categoryRepository->findAll()
         ]);
     }
-    
-    #[Route('/', name: 'index')]
-    
+
+    /**
+     * @Route("/", name="index")
+     */
     public function index(): Response
     {
         return $this->redirectToRoute('home.page');
     }
 }
+// class HomeController extends AbstractController
+// {
+//     #[Route('/home', name: 'home.page')]
+//     public function home(ArticleRepository $articleRepository): Response
+//     {
+//         return $this->render('pages/home.html.twig', [
+//             'articles'=> $articleRepository->findPublicArticle(4)
+//             //'categories' => $categoryRepository->findAll()
+//         ]);
+//     }
+    
+//     #[Route('/', name: 'index')]
+    
+//     public function index(): Response
+//     {
+//         return $this->redirectToRoute('home.page');
+//     }
+// }
     
